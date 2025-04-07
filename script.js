4328ea116c4e9aacd85b54c5e55c36b95cabe445
Adem <adem.benzineb.dev@gmail.com>
2025-04-07 21:06:18 +0100

    I ve selectedthe choises for the two players (Computer and player)
    I ve selected the computer choise clearly and the player's choise

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
 
 let Computer_score = 0;
 let Player_score = 0;
+let round = 0
 
-function Computer_choise(){
+function Computer_choise_number(){
     let max = 3;
     let min = 1;
-    let Comp_choise = Math.trunc(Math.random()*(max-min+1)+min);
-    console.log(Comp_choise)
-    return Comp_choise
+    let Comp_choise_n = Math.trunc(Math.random()*(max-min+1)+min);
+    console.log(Comp_choise_n)
+    return Comp_choise_n
 }
+
+function Computer_choise(){
+    let choise = "";
+    let n = Computer_choise_number();
+    if (n === 1){
+        choise = "rock"
+    }else if (n === 2){
+        choise = "paper"
+    }else{
+        choise = "scissors"
+    }
+    return choise
+}
+
+
 let p = 0
-function paper(){ p += 1}
+function paper(){ p += 1 ;verif();}
 
 let r = 0
-function rock(){ r += 1}
+function rock(){ r += 1 ;verif();}
 
 let s = 0
-function scissors(){ s += 1}
+function scissors(){ s += 1 ;verif();}
 
+function Player_choise(p,r,s){
+    let choise = ""
+    if(p === 1){
+        choise = "paper"
+    }else if(r === 1){
+        choise = "rock"
+    }else{
+        choise = "scissors"
+    }
+    return choise
+}
 
+function verif(){
+if (p > 0 | r > 0 | s > 0){
+    console.log(`Your choise is ${Player_choise(p,r,s)}`)
+    console.log(`The Computer choise is ${Computer_choise()}`)
+    p = 0
+    r = 0
+    s = 0
+}
+}
