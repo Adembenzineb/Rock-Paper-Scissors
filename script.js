1b957c1f967b7e98708af6917c55fe4e6285033a
Adem <adem.benzineb.dev@gmail.com>
2025-04-07 20:43:47 +0100

    added the computer choise and event listeners
    I ve maked the computer choose randomly from 1 to 3 and
    3 variables for each option for the player

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello world")
+
+let Computer_score = 0;
+let Player_score = 0;
+
+function Computer_choise(){
+    let max = 3;
+    let min = 1;
+    let Comp_choise = Math.trunc(Math.random()*(max-min+1)+min);
+    console.log(Comp_choise)
+    return Comp_choise
+}
+let p = 0
+function paper(){ p += 1}
+
+let r = 0
+function rock(){ r += 1}
+
+let s = 0
+function scissors(){ s += 1}
+
+
