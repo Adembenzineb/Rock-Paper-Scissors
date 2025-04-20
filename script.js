562a1753be9de5ab462ffcb00b05954db2d17fd8
Adem <adem.benzineb.dev@gmail.com>
2025-04-20 20:05:54 +0100

    Clear the js code and Add comments

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+//initalising scores and rounds
 let Computer_score = 0;
 let Player_score = 0;
 let round = 0;
@@ -17,6 +18,7 @@ const imagePaths = {
     }
 }
 
+// Picks a random number for the choise of the computer
 function Computer_choise_number(){
     let max = 3;
     let min = 1;
@@ -24,6 +26,8 @@ function Computer_choise_number(){
     return Comp_choise_n
 }
 
+
+//translating the random numbers into a choise (Rock/Paper/Scissors)
 function Computer_choise(){
     let choise = "";
     let n = Computer_choise_number();
@@ -38,13 +42,14 @@ function Computer_choise(){
     return choise
 }
 
+
+// Displays the computer choise on the screen
 function Compchoise(n){
     const choiseImg = document.querySelector('#chImg')
     choiseImg.src = imagePaths.choisesImgs[n]
 }
 
-
-
+//**these functions(rock/paper/scissors) are just to know the player's choise
 
 function paper(){
     p += 1 ;
@@ -63,6 +68,8 @@ function scissors(){ s += 1 ;
     console.log("the play ground function is running")
 }
 
+
+//translating the player's choise
 function Player_choise(p,r,s){
     let choise = ""
     if(p === 1){
@@ -75,6 +82,8 @@ function Player_choise(p,r,s){
     return choise
 }
 
+
+// Compares player and computer choices to determine the winner.
 function win(){
     let computer = Computer_choise();
     let player = Player_choise(p,r,s);
@@ -95,6 +104,7 @@ function win(){
     return res
 }
 
+//Adding score for the winner and initialising the choises 
 function playGround(){
     
     let winner = win() ;
